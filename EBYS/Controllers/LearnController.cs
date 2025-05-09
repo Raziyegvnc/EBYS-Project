eeb41fe2dd8ccede09af00dfd7dfa5c14b317eb0
Raziyegvnc <gvncraziye@outlook.com>
2025-05-09 09:37:28 +0300

    Add project files.

---
+++ EBYS/Controllers/LearnController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrainingWeb.Controllers
+{
+	public class LearnController : Controller
+	{
+		public IActionResult Index() => View();           
+		public IActionResult Slide1_EBYSNedir() => View();
+		public IActionResult Slide2_SistemeGiris() => View();
+		public IActionResult Slide3_BirimEvrak() => View();
+		public IActionResult Slide4_RotaIslemleri() => View();
+		public IActionResult Slide5_KayıtIslemleri() => View();
+		public IActionResult Slide6_IslemYaptıklarım() => View();
+		public IActionResult Slide7_PostaIslemleri() => View();
+		public IActionResult Slide8_IslemYapacaklarım() => View();
+		public IActionResult Slide9_EvrakArama () => View();
+		public IActionResult Slide10_DağıtımHavale() => View();
+	}
+}
---
+++ EBYS/Controllers/SimulasyonController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EBYS.Controllers
+{
+	public class SimulasyonController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View(); 
+		}
+
+		public IActionResult GelenEvrak()
+		{
+			return View(); 
+		}
+
+		public IActionResult GidenEvrakParaf()
+		{
+			return View();
+		}
+
+		public IActionResult OnayOluştur()
+		{
+			return View(); 
+		}
+
+		public IActionResult TebligatOluştur()
+		{
+			return View();
+		}
+
+		public IActionResult OlağanüstüEvrak()
+		{
+			return View();
+		}
+
+		public IActionResult RotaOluşturma()
+		{
+			return View();
+		}
+
+	}
+}
